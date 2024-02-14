f79974053b5bd64de3e434b55ff7a78450f481ee
halimdirgantara <halimdirgantara@gmail.com>
2024-02-14 15:18:44 +0700

    feat: add some column to user table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,6 +15,11 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
+            $table->string('phone')->nullable();
+            $table->string('nip')->unique();
+            $table->string('title')->nullable();
+            $table->string('grade')->nullable();
+            $table->string('avatar')->nullable();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
